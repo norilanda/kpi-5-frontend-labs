652d4b3b58f37aea5170acebd75c5d0634c849bd
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-11-05 10:30:31 +0200

    Add second task of laba5

--- laba5/script.js
+++ laba5/script.js
@@ -53,3 +53,83 @@ function createNewWindowInformation() {
 
   return htmlContent;
 }
+
+// ====================================================================
+// SECOND TASK
+// ====================================================================
+
+const colorPickerElement = document.getElementById("color-picker");
+const tableElement = document.getElementById("6x6-table");
+
+createTableDom(tableElement);
+
+const targetCellElement = document.getElementById("targetCell");
+const secondColumnCells = getAllColumnCells(tableElement, 1);
+
+// event listeners
+
+targetCellElement.addEventListener("mouseover", () => {
+  targetCellElement.style.backgroundColor = generateRandomColor();
+});
+
+targetCellElement.addEventListener("click", () => {
+  targetCellElement.style.backgroundColor = colorPickerElement.value;
+});
+
+targetCellElement.addEventListener("dblclick", () => {
+  secondColumnCells.forEach((cellElement) => {
+    cellElement.style.backgroundColor = colorPickerElement.value;
+  });
+});
+
+// functions
+
+function createTableDom(tableElement) {
+  const rowNumber = 6;
+  const columnNumber = 6;
+  const targetCellNumber = 2;
+
+  let counter = 1;
+
+  for (let i = 0; i < rowNumber; i++) {
+    const rowElement = document.createElement("tr");
+
+    for (let j = 0; j < columnNumber; j++) {
+      const cellElement = document.createElement("td");
+      cellElement.innerText = counter;
+      if (counter === targetCellNumber)
+        cellElement.setAttribute("id", "targetCell");
+
+      counter++;
+      rowElement.appendChild(cellElement);
+    }
+
+    tableElement.appendChild(rowElement);
+  }
+}
+
+function generateRandomColor() {
+  const red = generateRandomRGB();
+  const green = generateRandomRGB();
+  const blue = generateRandomRGB();
+
+  const hexColor = `#${red}${green}${blue}`;
+
+  return hexColor;
+
+  function generateRandomRGB() {
+    const randomColor = Math.floor(Math.random() * 256);
+    return randomColor.toString(16).padStart(2, "0");
+  }
+}
+
+function getAllColumnCells(tableElement, columnIndex) {
+  const cellElementList = [];
+
+  const rows = [...tableElement.rows];
+  rows.forEach((row) => {
+    cellElementList.push(row.cells[columnIndex]);
+  });
+
+  return cellElementList;
+}
